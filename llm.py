12bfccf5c7134a8093146a41dd666642cad1e0e6
yuhangwuai <yuhangcs.wu@gmail.com>
2024-06-30 21:23:43 +0100

    initialise

---
+++ llm.py
@@ -0,0 +1,132 @@
+from operator import itemgetter
+
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.memory import ConversationBufferMemory
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
+from langchain_core.messages import get_buffer_string
+from langchain_core.prompts import format_document
+from langchain.prompts.prompt import PromptTemplate
+
+
+condense_question = """Given the following conversation and a follow-up question, rephrase the follow-up question to be a standalone question.
+
+Chat History:
+{chat_history}
+
+Follow Up Input: {question}
+Standalone question:"""
+CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(condense_question)
+
+answer = """
+### Instruction:
+Use the following pieces of context to answer the question at the end. \
+If you don't know the answer, just say that you don't know, don't try to make up an answer. \
+Keep the answer as concise as possible.
+
+## Research:
+{context}
+
+## Question:
+{question}
+"""
+ANSWER_PROMPT = ChatPromptTemplate.from_template(answer)
+
+
+    #template="Document: {filename}, Content:\n{page_content}"
+DEFAULT_DOCUMENT_PROMPT = PromptTemplate.from_template(template="{page_content}")
+
+
+def _combine_documents(
+    docs, document_prompt=DEFAULT_DOCUMENT_PROMPT, document_separator="\n\n"
+):
+    doc_strings = [format_document(doc, document_prompt) for doc in docs]
+    return document_separator.join(doc_strings)
+
+
+memory = ConversationBufferMemory(
+    return_messages=True, output_key="answer", input_key="question"
+)
+
+
+def getStreamingChain(question: str, memory, llm, db):
+    retriever = db.as_retriever(search_kwargs={"k": 10})
+    loaded_memory = RunnablePassthrough.assign(
+        chat_history=RunnableLambda(
+            lambda x: "\n".join(
+                [f"{item['role']}: {item['content']}" for item in x["memory"]]
+            )
+        ),
+    )
+
+    standalone_question = {
+        "standalone_question": {
+            "question": lambda x: x["question"],
+            "chat_history": lambda x: x["chat_history"],
+        }
+        | CONDENSE_QUESTION_PROMPT
+        | llm
+    }
+
+    retrieved_documents = {
+        "docs": itemgetter("standalone_question") | retriever,
+        "question": lambda x: x["standalone_question"],
+    }
+
+    final_inputs = {
+        "context": lambda x: _combine_documents(x["docs"]),
+        "question": itemgetter("question"),
+    }
+
+    answer = final_inputs | ANSWER_PROMPT | llm
+
+    final_chain = loaded_memory | standalone_question | retrieved_documents | answer
+
+    return final_chain.stream({"question": question, "memory": memory})
+
+
+def getChatChain(llm, db):
+    retriever = db.as_retriever(search_kwargs={"k": 10})
+
+    loaded_memory = RunnablePassthrough.assign(
+        chat_history=RunnableLambda(memory.load_memory_variables)
+        | itemgetter("history"),
+    )
+
+    standalone_question = {
+        "standalone_question": {
+            "question": lambda x: x["question"],
+            "chat_history": lambda x: get_buffer_string(x["chat_history"]),
+        }
+        | CONDENSE_QUESTION_PROMPT
+        | llm
+    }
+
+    # Now we retrieve the documents
+    retrieved_documents = {
+        "docs": itemgetter("standalone_question") | retriever,
+        "question": lambda x: x["standalone_question"],
+    }
+
+    # Now we construct the inputs for the final prompt
+    final_inputs = {
+        "context": lambda x: _combine_documents(x["docs"]),
+        "question": itemgetter("question"),
+    }
+
+    # And finally, we do the part that returns the answers
+    answer = {
+        "answer": final_inputs
+        | ANSWER_PROMPT
+        | llm.with_config(callbacks=[StreamingStdOutCallbackHandler()]),
+        "docs": itemgetter("docs"),
+    }
+
+    final_chain = loaded_memory | standalone_question | retrieved_documents | answer
+
+    def chat(question: str):
+        inputs = {"question": question}
+        result = final_chain.invoke(inputs)
+        memory.save_context(inputs, {"answer": result["answer"]})
+
+    return chat
